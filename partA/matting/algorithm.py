120726c998d378e758a198572215e3f272d3675c
Jefferson Zhong <jefferson.zhong@mail.utoronto.ca>
2020-01-27 02:29:46 -0500

    Modify algorithm.py

--- partA/matting/algorithm.py
+++ partA/matting/algorithm.py
@@ -152,12 +152,7 @@ class Matting:
         #########################################
         ## PLACE YOUR CODE BETWEEN THESE LINES ##
         #########################################
-        image = self._images[key]
-        retval = cv.imwrite(fileName, image)
-        if not retval:
-            msg =  "Failed to write image with filename: " + fileName + ", key: " + key
-        else:
-            success = True
+
         #########################################
         return success, msg
 
